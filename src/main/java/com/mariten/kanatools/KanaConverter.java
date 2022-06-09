43a7e17d17e4de2da42e21fa9efb09cdcb14ba01
testerUse21 <106081205+testerUse21@users.noreply.github.com>
2022-06-09 19:06:41 +0530

    commited

--- src/main/java/com/mariten/kanatools/KanaConverter.java
+++ src/main/java/com/mariten/kanatools/KanaConverter.java
@@ -21,6 +21,8 @@ public class KanaConverter
     //// https://github.com/php/php-src/blob/a84e5dc37dc0ff8c313164d9db141d3d9f2b2730/ext/mbstring/mbstring.c#L3434
     public static final int OP_HAN_ASCII_TO_ZEN_ASCII      = 0x00000001;
     public static final int OP_HAN_LETTER_TO_ZEN_LETTER    = 0x00000002;
+     public static final int OP_HAN_ASCII_TO_ZEN_ASCII      = 0x00000001;
+    public static final int OP_HAN_LETTER_TO_ZEN_LETTER    = 0x00000002;
     public static final int OP_HAN_NUMBER_TO_ZEN_NUMBER    = 0x00000004;
     public static final int OP_HAN_SPACE_TO_ZEN_SPACE      = 0x00000008;
     public static final int OP_HAN_KATA_TO_ZEN_KATA        = 0x00000100;
